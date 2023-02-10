792f975e2b84262563f85fbe5d94531015be8b64
Evgeniy Kostromin <ewg.kostromin@yandexru>
2023-02-10 16:07:09 +0500

    19 задача исправлена и решена 3-мя спосабами

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -6,15 +6,58 @@
 
 Console.Clear();
 Console.Write("Введите пятизначное число: ");
-int num = int.Parse(Console.ReadLine()!);
-int a = num / 10000;
-int b = num % 10;
+// int num = int.Parse(Console.ReadLine()!); // num = 12345
+// int a1 = num / 10000; // a1 = 1 т.к мы отбрасываем последние 4 цифры
+// num -= a1 * 10000; // в num записываем 2345 <=> num = num % 1000 (2345)
+// int a2 = num / 1000; // уже от 2345 отбрасываем ри последнии цифры и a2 = 2
+// num -= a2 * 1000; // в num записываем 345
+// int a3 = num / 100; // c = 3
+// num -= a3 * 100; //  num = 45
+// int a4 = num / 10; // d = 4
+// int a5 = num % 10; // e = 5
 
-if (a == b)
+// if (a1 == a5 && a2 == a4)
+// {
+//   Console.Write($"Да");
+// }
+// else
+// {
+//   Console.Write($"Нет");
+// }
+
+// решаем через массив
+string num = Console.ReadLine()!;
+int palindr = num.Length;
+
+if (palindr == 5)
 {
-  Console.Write($"{num} -> Да");
-}
-else
+  if (num[0] == num[4] && num[1] == num[3] )
+  {
+    Console.Write("Да");
+  }else
+  {
+    Console.Write("Нет");
+  }
+}else
 {
-  Console.Write($"{num} -> Нет");
+  Console.Write("Неверное число");
 }
+
+
+// универсальное решение для всех чисел
+// int Revers(int num){
+//   int revers = 0;
+//   while (num > 0)
+//   {
+//     revers = revers * 10 + num % 10;
+//     num /= 10;
+//   }
+//   return revers;
+// }
+// if (num == Revers(num))
+// {
+//   Console.Write($"Да");
+// }else
+// {
+//   Console.Write($"Нет");
+// }
